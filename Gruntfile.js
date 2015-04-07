4b9c2239a5b151fc91571a5eea57d27e0739d9f9
Scott Davis <stdavis@utah.gov>
2015-04-07 15:34:43 -0600

    better grunt task loading

--- Gruntfile.js
+++ Gruntfile.js
@@ -142,7 +142,7 @@ module.exports = function(grunt) {
         };
     }
 
-    // Project configuration.
+    require('load-grunt-tasks')(grunt);
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         arcgis_press: {
@@ -380,14 +380,6 @@ module.exports = function(grunt) {
         }
     });
 
-    // Loading dependencies
-    for (var key in grunt.file.readJSON('package.json').devDependencies) {
-        if (key !== 'grunt' && key.indexOf('grunt') === 0) {
-            grunt.loadNpmTasks(key);
-        }
-    }
-
-    // Default task.
     grunt.registerTask('default', [
         'jasmine:main:build',
         'newer:jshint:main',
