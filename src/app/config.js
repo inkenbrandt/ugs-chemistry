281c33ddd5287ccdd8c1f9997af1209811213ccd
Scott Davis <stdavis@utah.gov>
2015-09-28 15:26:21 -0600

    Release v0.5.1

--- src/app/config.js
+++ src/app/config.js
@@ -52,7 +52,7 @@ define([
 
         // version.: String
         //      The version number.
-        version: '0.5.0',
+        version: '0.5.1',
 
         // apiKey: String
         //      The api key used for services on api.mapserv.utah.gov
